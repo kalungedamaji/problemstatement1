7060652c74dba87d1ce91b2c5a0611e7e8a6a0b4
VISHAL RAJ <45869380+Vrajsingh@users.noreply.github.com>
2022-01-12 16:26:02 +0530

    Encapsulated the Shopping Cart and Product.

--- src/oops/Product.java
+++ src/oops/Product.java
@@ -3,6 +3,22 @@ package oops;
 import java.math.BigDecimal;
 
 public class Product {
-    String name;
-    BigDecimal price;
+     private String name;
+     private BigDecimal price;
+
+    public  void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/oops/ShoppingCart.java
+++ src/oops/ShoppingCart.java
@@ -3,15 +3,37 @@ package oops;
 import java.math.BigDecimal;
 
 public class ShoppingCart {
-    int numberOfProducts = 0;
-    Product product;
+     private int numberOfProducts = 0;
+     private Product product;
 
-    public void addProducts(Product product, int numberOfProducts) {
-        this.numberOfProducts = numberOfProducts;
-        this.product = product;
-    }
+        public void setAddProducts (Product product, int numberOfProducts){
+            this.product = product;
+            this.numberOfProducts = numberOfProducts;
+        }
 
-    public BigDecimal calculateTotalPrice() {
-        return (product.price.multiply(BigDecimal.valueOf(numberOfProducts)).setScale(2, BigDecimal.ROUND_HALF_UP)) ;
-    }
+        public void setNumberOfProducts(int numberOfProducts) {
+            if(numberOfProducts >= 0) {
+                this.numberOfProducts = numberOfProducts;
+            }
+            else {
+                this.numberOfProducts = 0;
+            }
+        }
+
+        public Product getProduct() {
+            return this.product;
+        }
+
+
+        public int getNumberOfProducts() {
+            return this.numberOfProducts;
+        }
+
+
+        public BigDecimal calculateTotalPrice () {
+            return getProduct().getPrice().multiply(BigDecimal.valueOf(getNumberOfProducts())).setScale(2, BigDecimal.ROUND_HALF_UP);
+        }
+
+
 }
+
