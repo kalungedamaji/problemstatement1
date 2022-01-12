b04fc540e6cd011893cabacc06883d8064fd9fca
Parth Deshmukh <parthdeshmukh06@gmail.com>
2022-01-12 16:39:22 +0530

    Encapsulated Shopping cart and Product.

--- src/oops/Product.java
+++ src/oops/Product.java
@@ -3,6 +3,22 @@ package oops;
 import java.math.BigDecimal;
 
 public class Product {
-    String name;
-    BigDecimal price;
+    private String name;
+    private BigDecimal price;
+
+    public String getName(){
+        return this.name;
+    }
+    public BigDecimal getPrice(){
+        return this.price;
+    }
+
+    public void setPrice(BigDecimal price){
+        this.price=price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
--- src/oops/ShoppingCart.java
+++ src/oops/ShoppingCart.java
@@ -3,15 +3,37 @@ package oops;
 import java.math.BigDecimal;
 
 public class ShoppingCart {
-    int numberOfProducts = 0;
-    Product product;
+    private int numberOfProducts = 0;
+    private Product product;
 
     public void addProducts(Product product, int numberOfProducts) {
-        this.numberOfProducts = numberOfProducts;
-        this.product = product;
+        this.setNumberOfProducts(numberOfProducts);
+        this.setProduct(product);
     }
 
     public BigDecimal calculateTotalPrice() {
-        return (product.price.multiply(BigDecimal.valueOf(numberOfProducts)).setScale(2, BigDecimal.ROUND_HALF_UP)) ;
+        return getProduct().getPrice().multiply(BigDecimal.valueOf(getNumberOfProducts())).setScale(2, BigDecimal.ROUND_HALF_UP);
+    }
+
+    public void setNumberOfProducts(int numberOfProducts) {
+        if(numberOfProducts>=0){
+            this.numberOfProducts = numberOfProducts;
+        }
+        else{
+            this.numberOfProducts=0;
+        }
+        //this.numberOfProducts = numberOfProducts;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getNumberOfProducts() {
+        return numberOfProducts;
+    }
+
+    public Product getProduct() {
+        return product;
     }
 }
